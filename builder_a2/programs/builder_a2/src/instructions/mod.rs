7a9de1d89f52ece84a9b2a0dcf96a44aa252d0ba
AaroneGeorge <aaroneofficial1879@gmail.com>
2026-05-19 11:42:07 +0530

    basio code added; lib, state, error, instructions mod

---
+++ builder_a2/programs/builder_a2/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+pub mod create_escrow;
+pub mod accept_escrow;
+pub mod refund_escrow;
+
+pub use create_escrow::*;
+pub use accept_escrow::*;
+pub use refund_escrow::*;
--- builder_a2/programs/builder_a2/src/lib.rs
+++ builder_a2/programs/builder_a2/src/lib.rs
@@ -1,4 +1,11 @@
-use anchor_lang::prelude::*;
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
 
 declare_id!("EsfzrVNn3fuSacJw55y1AamEa9NCPudV3hzEM8K7Kf7i");
 
@@ -6,11 +13,20 @@ declare_id!("EsfzrVNn3fuSacJw55y1AamEa9NCPudV3hzEM8K7Kf7i");
 pub mod builder_a2 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    #[instruction(discriminator = 0)]
+    pub fn create_escrow(ctx: Context<Create>, seed: u64, receive: u64, deposit: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)
+    }
+
+    #[instruction(discriminator = 1)]
+    pub fn accept_escrow(ctx: Context<Accept>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
+
+    #[instruction(discriminator = 2)]
+    pub fn refund_escrow(ctx: Context<Refund>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
