36a6e8a63f82440f445fb91b230940c60b2eca28
AaroneGeorge <aaroneofficial1879@gmail.com>
2026-06-09 15:09:35 +0530

    instructions: create collection and mint asset added

---
+++ builders3_a1/programs/builders3_a1/src/instructions/create_collection.rs
@@ -0,0 +1,49 @@
+use anchor_lang::prelude::*;
+use mpl_core::{instructions::CreateCollectionV2CpiBuilder, ID as MPL_CORE_ID};
+
+#[derive(Accounts)]
+pub struct CreateCollection<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(mut)]
+    pub collection: Signer<'info>,
+
+    #[account(
+        seeds = [b"update_authority", collection.key().as_ref()],
+        bump,
+    )]
+    pub update_authority: UncheckedAccount<'info>, //it's used to sign the CPI to Metaplex
+
+    #[account(address = MPL_CORE_ID)]
+    pub mpl_core_program: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+pub fn handler(ctx: Context<CreateCollection>, name: String, uri: String) -> Result<()> {
+    // Stores the collection's public key (needed to derive the bump later)
+    let collection_key = ctx.accounts.collection.key();
+
+    let signer_seeds = &[
+        b"update_authority",
+        collection_key.as_ref(),
+        &[ctx.bumps.update_authority],
+    ];
+
+    CreateCollectionV2CpiBuilder::new(&ctx.accounts.mpl_core_program.to_account_info())
+        .collection(&ctx.accounts.collection.to_account_info())
+        .payer(&ctx.accounts.payer.to_account_info())
+        .update_authority(Some(&ctx.accounts.update_authority.to_account_info()))
+        .system_program(&ctx.accounts.system_program.to_account_info())
+        .name(name)
+        .uri(uri)
+        .invoke_signed(&[signer_seeds])?;
+
+    Ok(())
+}
+
+//   .collection() = the NFT collection being created
+//   .payer() = who pays for the creation
+//   .update_authority() = who can update this collection later (the PDA we created)
+//   .name() / .uri() = metadata for the collection
